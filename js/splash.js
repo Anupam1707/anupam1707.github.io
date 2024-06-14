a398ab8bff82154c2ed53b7111cfae02ad10f651
Anupam Kanoongo <programer.tiak@gmail.com>
2024-06-14 16:16:37 +0530

    Updated Splash Screen

--- js/splash.js
+++ js/splash.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
   const splash = document.getElementById('splashScreen');
   setTimeout(function() {
     splash.classList.add('hide');
-  }, 3000); // 3000 milliseconds = 3 seconds
+  }, 4000); // 3000 milliseconds = 3 seconds
 });
