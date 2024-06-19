8b8552ba60c2cbe9f1f7ea9760769e07ad740cb8
Anupam Kanoongo <programer.tiak@gmail.com>
2024-06-19 21:06:08 +0530

    About Page Creation

---
+++ js/about.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('nav ul li a');
+  
+  links.forEach(link => {
+      link.addEventListener('click', function(e) {
+          e.preventDefault();
+          const targetId = this.getAttribute('href').substring(1);
+          document.getElementById(targetId).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+});
