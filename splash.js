9fa4211147574080ea1aefe4eadd8650c135eae2
Anupam Kanoongo <programer.tiak@gmail.com>
2024-07-07 19:48:25 +0530

    July Patch

--- splash.js
+++ splash.js
@@ -1,7 +1,8 @@
-// JavaScript for splash screen
 document.addEventListener('DOMContentLoaded', function() {
   const splash = document.getElementById('splashScreen');
   setTimeout(function() {
     splash.classList.add('hide');
-  }, 4000); // 3000 milliseconds = 3 seconds
+    // Redirect to another page after hiding the splash screen
+    window.location.href = 'home'; // Replace with your target URL
+  }, 4000); // 4000 milliseconds = 4 seconds
 });
