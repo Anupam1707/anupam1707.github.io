7280b97a6c0dcd55dfcaab990a53dea775911878
Anupam Kanoongo <programer.tiak@gmail.com>
2024-07-08 13:20:37 +0530

    NodeJs Trial

---
+++ scripts.js
@@ -0,0 +1,17 @@
+fetch('https://portfolio-backend-web.onrender.com/certificates')
+    .then(response => response.json())
+    .then(data => displayCertificates(data));
+
+function displayCertificates(certificates) {
+    const container = document.getElementById('certificates-container');
+    certificates.forEach((certificate, index) => {
+        const certDiv = document.createElement('div');
+        certDiv.className = 'certificate';
+        certDiv.innerHTML = `
+            <img src="${certificate.imageUrl}" alt="${certificate.title}" style="width:100%">
+            <h3>${certificate.title}</h3>
+            <p>${certificate.description}</p>
+        `;
+        container.appendChild(certDiv);
+    });
+}
