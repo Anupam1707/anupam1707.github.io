f23f108210d95835396f02ebe65230d11c9a9a36
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-08 23:51:40 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 fetch('https://portfolio-backend-api-nwhk.onrender.com/certificates')
     .then(response => response.json())
-    .then(data => displayCertificates(data));
+    .then(data => displayCertificates(data))
+    .catch(error => console.error('Error fetching certificates:', error));
 
 function displayCertificates(certificates) {
     const container = document.getElementById('certificates-container');
