6aa8595d6349f118ba2e48d0c534446bd57e5715
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-09 19:47:42 +0530

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loadingIndicator = document.getElementById('loading');
+    const projectsContainer = document.getElementById('projects-container');
+
+    fetch('https://portfolio-backend-api-nwhk.onrender.com/projects')  // Replace with your actual backend URL for projects
+        .then(response => response.json())
+        .then(projects => {
+            displayProjects(projects);
+            loadingIndicator.style.display = 'none';
+        })
+        .catch(error => {
+            console.error('Error fetching projects:', error);
+            loadingIndicator.innerText = 'Failed to load projects.';
+        });
+
+    function displayProjects(projects) {
+        projectsContainer.innerHTML = ''; // Clear the container first
+        for (let i = projects.length - 1; i >= 0; i--) {
+            const certificate = projects[i];
+            const projectElement = document.createElement('div');
+            projectElement.className = 'project';
+            projectElement.innerHTML = `
+                <img src="${project.imageUrl}" alt="${project.title}">
+                <div class="project-content">
+                    <h2>${project.title}</h2>
+                    <h3>${project.date}</h3>
+                    <p>${project.description}</p>
+                    <button onclick="window.location.href='${project.link}'">View Project</button>
+                </div>
+            `;
+            projectsContainer.appendChild(projectElement);
+        });
+    }
+});
