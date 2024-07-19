cb97e3bfcddc37f7f5e0e3de9fec9266169d3dee
Anupam Kanoongo <programer.tiak@gmail.com>
2024-07-19 23:26:39 +0530

    Added Blog Content

---
+++ blog.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const title = urlParams.get('title');
+
+  fetch('https://portfolio-backend-api-nwhk.onrender.com/blogs') // Replace with your actual backend URL for blogs
+    .then(response => response.json())
+    .then(blogs => {
+      const blog = blogs.find(blog => blog.title === title);
+      if (blog) {
+        document.getElementById('blog-title').innerText = blog.title;
+        document.getElementById('blog-image').src = blog.imageurl;
+        document.getElementById('blog-content').innerHTML = blog.content.replace(/\n/g, '<br>'); // Preserving line breaks
+      } else {
+        document.getElementById('blog-title').innerText = 'Blog not found';
+      }
+    })
+    .catch(error => console.error('Error fetching blog:', error));
+});
---
+++ blogs.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loadingIndicator = document.getElementById('loading');
+    const blogListContainer = document.getElementById('blog-list-container');
+  
+    fetch('https://portfolio-backend-api-nwhk.onrender.com/blogs') // Replace with your actual backend URL for blogs
+      .then(response => response.json())
+      .then(blogs => {
+        displayBlogs(blogs);
+        loadingIndicator.style.display = 'none';
+      })
+      .catch(error => {
+        console.error('Error fetching blogs:', error);
+        loadingIndicator.innerText = 'Failed to load blogs.';
+      });
+  
+    function displayBlogs(blogs) {
+      blogListContainer.innerHTML = ''; // Clear the container first
+      for (let i = blogs.length - 1; i >= 0; i--) {
+        const blog = blogs[i];
+        const blogElement = document.createElement('div');
+        blogElement.className = 'blog-post';
+        blogElement.innerHTML = `
+          <a href="blog.html?title=${encodeURIComponent(blog.title)}">
+            <img src="${blog.imageurl}" alt="${blog.title}" class="blog-image">
+          </a>
+          <div class="blog-content">
+            <h2>${blog.title}</h2>
+            <p>${blog.content}</p>
+          </div>
+        `;
+        blogListContainer.appendChild(blogElement);
+      }
+    }
+  });
+  
