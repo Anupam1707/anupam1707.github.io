8a5d17b3f3663fdce2cedddbb142dafb7cc62026
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-20 13:11:49 +0530

    Update blog.js

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const title = urlParams.get('query');
+  const title = urlParams.get('query'); // Update parameter name if needed
+
+  if (!title) {
+    // Redirect if title is empty
+    window.location.href = 'blogs'; // Redirect to the blog home or another appropriate page
+    return; // Exit the script to prevent further execution
+  }
 
   fetch('https://portfolio-backend-api-nwhk.onrender.com/blogs') // Replace with your actual backend URL for blogs
     .then(response => response.json())
@@ -9,8 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
       if (blog) {
         document.getElementById('blog-title').innerText = blog.title;
         document.getElementById('blog-image').src = blog.imageurl;
-        document.getElementById('blog-date').innerText = blog.date
-        document.getElementById('blog-author').innerText = "By " + blog.author
+        document.getElementById('blog-date').innerText = blog.date;
+        document.getElementById('blog-author').innerText = "By " + blog.author;
         document.getElementById('blog-content').innerHTML = blog.content.replace(/\n/g, '<br>'); // Preserving line breaks
       } else {
         document.getElementById('blog-title').innerText = 'Blog not found';
