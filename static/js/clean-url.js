a4666ac85836fb2d3fa049a10f787296afa5e392
Anupam1707 <programmer.tiak@gmail.com>
2026-03-02 23:52:08 +0530

    blogs

--- static/js/clean-url.js
+++ static/js/clean-url.js
@@ -1,8 +1,10 @@
-// Clean URL - Remove .html from address bar
+// Clean URL - Remove .html from address bar while preserving query parameters
 (function() {
     const path = window.location.pathname;
     if (path.endsWith('.html')) {
         const cleanPath = path.replace('.html', '');
-        window.history.replaceState(null, '', cleanPath);
+        const search = window.location.search; // Preserve query parameters
+        const hash = window.location.hash; // Preserve hash
+        window.history.replaceState(null, '', cleanPath + search + hash);
     }
 })();
